322dc25326c8c586bec799f9156466bee9010f52
IgorIlyaguev <non_2002@mail.ru>
2025-03-22 14:30:18 +0300

    install and configure linter and static analyzer

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -9,10 +9,14 @@
  * file that was distributed with this source code.
  */
 
+use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
